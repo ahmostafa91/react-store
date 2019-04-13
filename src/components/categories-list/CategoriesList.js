eefc0b77e1d353879f5c446af602951e297f6b9a
ahmed <ahmedmostafa05031991@gmail.com>
2019-04-13 23:52:56 +0200

    add category filter

--- src/components/categories-list/CategoriesList.js
+++ src/components/categories-list/CategoriesList.js
@@ -3,6 +3,8 @@ import './styleCategories.css';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { getAllCateg } from '../../actions/categoriesAction';
+import { setFilter } from './../../actions/filterAction';
+import { CATEGORY_FILTER } from '../../constants/filtersTypes';
 
 class CategoryList extends Component {
 
@@ -13,7 +15,10 @@ class CategoryList extends Component {
     }
 
     render(){
-        const divs = this.props.categories.map(e => <div key={e.id} onClick={() => {}}>{e.name}</div>);
+        const divs = this.props.categories.map(e => <div key={e.id} onClick={() => {this.props.setFilter({
+            type: CATEGORY_FILTER,
+            value: e.id
+        });}}>{e.name}</div>);
 
         return (
             <React.Fragment>
@@ -31,7 +36,8 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
-        getAllCategs: bindActionCreators(getAllCateg, dispatch)
+        getAllCategs: bindActionCreators(getAllCateg, dispatch),
+        setFilter: bindActionCreators(setFilter, dispatch)
     }
 }
 
--- src/constants/filtersTypes.js
+++ src/constants/filtersTypes.js
@@ -1,1 +1,2 @@
 export const COLOR_FILTER = 'COLOR_FILTER';
+export const CATEGORY_FILTER = 'CATEGORY_FILTER';
