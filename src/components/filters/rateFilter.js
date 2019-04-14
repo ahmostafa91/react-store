f160b79049251f1e0aeec433848343bb58c8f215
ahmed <ahmedmostafa05031991@gmail.com>
2019-04-14 04:08:45 +0200

    attached ratting filter in filter reducer

--- src/components/filters/rateFilter.js
+++ src/components/filters/rateFilter.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import {Rating} from 'primereact/rating';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { setFilter } from './../../actions/filterAction';
+import { RATTING_FILTER } from '../../constants/filtersTypes';
 
 class RatingFilter extends Component {
     state = { 
@@ -11,11 +15,29 @@ class RatingFilter extends Component {
             <React.Fragment>
 
             <h3 className="first">Stars Rate:  {this.state.val1}</h3>
-            <Rating value={this.state.val1} onChange={(e) => this.setState({val1: e.value})} />
+            <Rating value={this.state.val1} cancel={false} onChange={(e) => {
+                this.setState({val1: e.value});
+                this.props.setFilter({
+                    type: RATTING_FILTER,
+                    value: e.value
+                });
+            }} />
 
             </React.Fragment>
         )
     }
 }
 
-export default RatingFilter;
+function mapStateToProps(state) {
+    return {
+        products: state.products // get products array to get the colors
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        setFilter: bindActionCreators(setFilter, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RatingFilter);
--- src/constants/filtersTypes.js
+++ src/constants/filtersTypes.js
@@ -1,2 +1,3 @@
 export const COLOR_FILTER = 'COLOR_FILTER';
 export const CATEGORY_FILTER = 'CATEGORY_FILTER';
+export const RATTING_FILTER = 'RATTING_FILTER';
